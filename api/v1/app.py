934e2d759ffcd9e9f06d7b877b585ff21566718c
johnyREx <johnkwofie99@icloud.com>
2024-01-07 02:18:09 -0800

    fixed api faults

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
+"""Flask web application
 """
-Script uses blueprint object for routing the application
-"""
-from flask import Flask, jsonify, make_response
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(error):
-    """
-    function closes the db when called
+def teardown(error):
+    """Clean-up method
     """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(e):
+def not_found(error):
+    """Custom 404 error
     """
-    404 error handler
-    """
-    return make_response(jsonify({"error": 'Not found'}), 404)
-
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = 5000
-
-    app.run(host=host, port=port, threaded=True)  # type: ignore
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
